f851811f6d9c65c19e7fc5ef953414dd07e4095a
Corentin Mercier <corentin@mercier.link>
2019-07-09 17:37:45 +0700

    Init balance command

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from telegram.ext import Updater, CommandHandler
-# import sqlite3
+import sqlite3
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+# Init the database
+# cursor.execute("""CREATE TABLE users(
+#     id INTEGER,
+#     balance REAL
+# )""")
+# conn.commit()
 
 
 TEST_ADDRESS = 'bitcoincash:qp07y2dy5jvcpfgssfalajytm3xg7yz5fye2gu5cz9'
@@ -14,6 +27,18 @@ def deposit(bot, update):
     update.message.reply_text(TEST_ADDRESS)
 
 
+def balance(bot, update):
+    # conn = sqlite3.connect('db.sqlite3')
+    # cursor = conn.cursor()
+    # query = ('SELECT balance FROM users WHERE id={}').format(
+    #                                             update.message.from_user.id)
+    # balance = cursor.execute(query)
+
+    balance = 0
+
+    update.message.reply_text('You have: ' + str(balance) + ' BCH')
+
+
 def withdraw(bot, update):
     update.message.reply_text('I cannot do that yet 😅')
 
@@ -23,19 +48,11 @@ updater = Updater('892772409:AAGQk_Fyz3Uelwvhoq8yUmRXPUuTxnFFIfY')
 # Commands
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('deposit', deposit))
+updater.dispatcher.add_handler(CommandHandler('balance', balance))
 updater.dispatcher.add_handler(CommandHandler('withdraw', withdraw))
 
 updater.start_polling()
 updater.idle()
 
-# conn = sqlite3.connect('db.sqlite3')
-
-# cursor = conn.cursor()
-
-# # cursor.execute("""CREATE TABLE users(
-# #     username TEXT,
-# #     balance REAL
-# # )""")
-# # conn.commit()
-
-# conn.close()
+# Update this later
+conn.close()
