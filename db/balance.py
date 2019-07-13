a7864396c5b732049442aa9b66dee8580fe7be8b
Corentin Mercier <corentin@mercier.link>
2019-07-13 22:11:48 +0700

    Refactor

--- db/balance.py
+++ db/balance.py
@@ -7,33 +7,24 @@ from settings import DATABASE_PINK
 
 def get_balance(username):
     """ Returns the balance (float) or [username] """
-    db.connect(reuse_if_open=True)
     balance = User.get(User.username == username).balance
-    db.close()
-
     return balance
 
 
 def get_address(username):
     """ Returns the Bitcoin Cash address (str) of [username] """
-    db.connect()
     address = User.get(User.username == username).bch_address
-    db.close()
-
     return address
 
 
 def update_balance(username, amount, operator):
     """ Updates (increase or decrease) the user's balance """
-    db.connect(reuse_if_open=True)
     user = User.get(User.username == username)
     if operator == '+':
         user.balance += float(amount)
     else:
         user.balance -= float(amount)
     user.save()
-    db.close()
-
     return True
 
 
@@ -46,14 +37,11 @@ def deduct(username, amount):
     """ Removes [amount] BCH to [username] if the remaining balance
     is positive.
     """
-    db.connect(reuse_if_open=True)
     user = User.get(User.username == username)
     remaining_balance = user.balance - float(amount)
-    db.close()
 
     if remaining_balance < 0:
         return 'Insufficient balance.'
-
     return update_balance(username, amount, operator='-')
 
 
