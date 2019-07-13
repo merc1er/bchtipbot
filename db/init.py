bea42cd683d9703416e0a84d11be75515dc6326e
Corentin Mercier <corentin@mercier.link>
2019-07-13 21:03:17 +0700

    Create user peeweed

--- db/init.py
+++ db/init.py
@@ -1,32 +1,23 @@
 import os.path
 from bitcash import Key
 from settings import DATABASE_PINK
+from .models import db, User
 
 
 def create_user(username):
     """ Checks if a Telegram user is present in the database.
     Returns True if a user is created, False otherwise.
     """
-    conn = sqlite3.connect(DATABASE_PINK)
-    cursor = conn.cursor()
-
-    query = ('SELECT * FROM users WHERE username="' + username + '"')
-    entry = cursor.execute(query).fetchone()
-
-    # if user is not present in the database
-    if not entry:
-        key = Key()
-        context = (
-            username,
-            0,  # initial balance
-            key.address,
-            key.to_wif(),
+    db.connect(reuse_if_open=True)
+    key = Key()
+    try:
+        User.create(
+            username=username,
+            bch_address=key.address,
+            wif=key.to_wif()
         )
-
-        cursor.execute('INSERT INTO users VALUES (?, ?, ?, ?)', context)
-        conn.commit()
-        conn.close()
+        db.close()
         return True
-
-    conn.close()
-    return False
+    except:
+        db.close()
+        return False
