a189d4b7911ca837d046a7dd616e98cc1b000a91
Corentin Mercier <corentin@mercier.link>
2019-09-12 16:17:36 +1000

    Added DEBUG env var

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
-# Define settings
+import os
 
+
+# Define settings
+DEBUG = os.environ['DEBUG'] == 'True'  # because DEBUG is a string
 
 DATABASE_PINK = 'db.sqlite3'
 TELEGRAM_TOKEN = '892772409:AAGQk_Fyz3Uelwvhoq8yUmRXPUuTxnFFIfY'
