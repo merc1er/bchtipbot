d97248a7256fbe81787091b1590548054aa47e30
Corentin Mercier <corentin@mercier.link>
2019-09-12 16:48:45 +1000

    Added Postgres connection peewee

--- db/models.py
+++ db/models.py
@@ -1,8 +1,21 @@
 from peewee import *
-from settings import DATABASE_PINK
+from settings import DEBUG, DATABASE_PINK
+import os
+from urllib.parse import urlparse, uses_netloc
 
 
-db = SqliteDatabase(DATABASE_PINK)
+if DEBUG:
+    db = SqliteDatabase(DATABASE_PINK)
+else:
+    uses_netloc.append('postgres')
+    url = urlparse(os.environ['DATABASE_URL'])
+    db = PostgresqlDatabase(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port,
+    )
 
 class User(Model):
     username = CharField(max_length=30, unique=True)
