1870b79cd93d8198548ec960a6a1b186458696df
Corentin Mercier <corentin@mercier.link>
2019-11-18 20:17:28 +0200

    Added check.username_is_valid() tests

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -17,5 +17,23 @@ class TestAmountIsValid(unittest.TestCase):
         self.assertTrue(checks.amount_is_valid(1.09))
 
 
+class TestUsernameIsValid(unittest.TestCase):
+
+    def test_other_types(self):
+        # Only strings will be passed through Telegram API
+        # e.g. 1 will be '1'
+        pass
+
+    def test_bad_usernames(self):
+        self.assertFalse(checks.username_is_valid('baduname'))
+        self.assertFalse(checks.username_is_valid('b@dun@me'))
+        self.assertFalse(checks.username_is_valid('@123'))
+        long_uname = '@iamaveryveryveryveryverylongusername'
+        self.assertFalse(checks.username_is_valid(long_uname))
+
+    def test_good_username(self):
+        self.assertTrue(checks.username_is_valid('@merc1er'))
+
+
 if __name__ == '__main__':
-    unittest.main
+    unittest.main()
