b7986fd26be8767edab6b7408d0cae3f61812f7a
Corentin Mercier <corentin@mercier.link>
2019-11-18 20:07:40 +0200

    Added more test cases

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -7,6 +7,15 @@ class TestAmountIsValid(unittest.TestCase):
     def test_negative_amount(self):
         self.assertFalse(checks.amount_is_valid(-1))
 
+    def test_string(self):
+        self.assertFalse(checks.amount_is_valid('blablah'))
+
+    def test_correct_integer(self):
+        self.assertTrue(checks.amount_is_valid(687))
+
+    def test_correct_float(self):
+        self.assertTrue(checks.amount_is_valid(1.09))
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main
