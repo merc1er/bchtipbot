52b1bab9faa8544ad7996a3b0a665a9f9cd6f8b5
Corentin Mercier <corentin@mercier.link>
2019-11-18 20:04:18 +0200

    Added if __name__ ...

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -6,3 +6,7 @@ class TestAmountIsValid(unittest.TestCase):
 
     def test_negative_amount(self):
         self.assertFalse(checks.amount_is_valid(-1))
+
+
+if __name__ == '__main__':
+    unittest.main()
