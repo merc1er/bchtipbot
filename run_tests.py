e94e9a642dbecadb6ad59bcc1489614fd25d64a6
Corentin Mercier <corentin@mercier.link>
2020-01-11 16:31:29 +0100

    Made PEP8 compliant

--- run_tests.py
+++ run_tests.py
@@ -2,8 +2,9 @@ import subprocess
 
 
 def main():
-    subprocess.call(['python', '-m', 'unittest','tests.test_checks'])
+    subprocess.call(['python', '-m', 'unittest', 'tests.test_checks'])
     print('\n\nTests completed.')
+
 
 if __name__ == '__main__':
     main()
