76c6fb9efb065c2eb2a95deeefb9c19e477fda4a
Corentin Mercier <corentin@mercier.link>
2020-01-15 21:27:14 +0700

    Added error type - PEP8 compliance

--- tipbot/settings.py
+++ tipbot/settings.py
@@ -4,20 +4,20 @@ import os
 # Define settings
 try:
     DEBUG = os.environ['DEBUG'] == 'True'  # because DEBUG is a string
-except:
+except KeyError:
     DEBUG = True
 
 
 try:
     TOKEN = os.environ['TOKEN']
-except:
+except KeyError:
     TOKEN = '892772409:AAGQk_Fyz3Uelwvhoq8yUmRXPUuTxnFFIfY'
 
 
 # Your output BCH address
 try:
     FEE_ADDRESS = os.environ['FEE_ADDRESS']
-except:
+except KeyError:
     FEE_ADDRESS = 'bitcoincash:qr02vc2t5yr9fe4ujdpkg99d5d0dgxstfqtgxg7umu'
 # The fee you want to charge (0.01 is 1%)
 FEE_PERCENTAGE = 0.01
