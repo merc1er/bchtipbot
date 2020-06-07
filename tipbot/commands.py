f585040d6484066a8117b997b72dee04abf2b767
Corentin Mercier <corentin@mercier.link>
2020-06-07 11:29:01 +0700

    Add 1 cent fee rule comment

--- tipbot/commands.py
+++ tipbot/commands.py
@@ -169,7 +169,7 @@ def tip(bot, update, args):
     fee = float(amount) * FEE_PERCENTAGE
     sent_amount = float(amount) - 0.01
 
-    if fee < 0.01:
+    if fee < 0.01:  # if the bot fee is less than 1 cent, don't take any fee
         outputs = [
             (recipient_address, sent_amount, "usd"),
         ]
