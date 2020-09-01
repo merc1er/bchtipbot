2066eed544174a7ac3729c49c11d586b6906bd9d
Corentin Mercier <corentin@mercier.link>
2020-09-01 23:36:57 +0700

    Add tests for get_game_overview()

--- tests/test_playmo.py
+++ tests/test_playmo.py
@@ -8,3 +8,11 @@ class TestSession(unittest.TestCase):
         keys = playmo.init_session()
         self.assertTrue(isinstance(keys.get("session_id"), int))
         self.assertEqual(len(keys.get("session_key")), 64)
+
+
+class TestGameOverview(unittest.TestCase):
+
+    def test_get_game_overview(self):
+        info = playmo.get_game_overview(224)
+        self.assertEqual(info.get("title"), "Fortnite Mini Battle Royale")
+        self.assertEqual(info.get("entrance_fee"), 500)
